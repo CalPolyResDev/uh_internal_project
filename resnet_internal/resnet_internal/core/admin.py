5746143fbfebc938aa48c6e28a549cd4ae307c94
Alexander Kavanaugh <akavanau@calpoly.edu>
2014-06-26 12:48:02 -0700

    added rnin user admin

--- resnet_internal/resnet_internal/core/admin.py
+++ resnet_internal/resnet_internal/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import CSDMapping, StaffMapping
+from .models import CSDMapping, StaffMapping, ResNetInternalUser
 
 
 class CSDMappingAdmin(admin.ModelAdmin):
@@ -10,5 +10,14 @@ class CSDMappingAdmin(admin.ModelAdmin):
 class StaffMappingAdmin(admin.ModelAdmin):
     list_display = ('staff_title', 'staff_alias', 'staff_name')
 
+
+class ResNetInternalUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'is_active',
+                    'is_net_admin', 'is_telecom', 'is_tag', 'is_rn_staff', 'is_technician',
+                    'is_new_tech', 'orientation_complete', 'is_developer')
+    list_filter = ('is_active', 'is_net_admin', 'is_telecom', 'is_tag', 'is_rn_staff',
+                   'is_technician', 'is_new_tech', 'is_developer')
+
 admin.site.register(CSDMapping, CSDMappingAdmin)
 admin.site.register(StaffMapping, StaffMappingAdmin)
+admin.site.register(ResNetInternalUser, ResNetInternalUserAdmin)
