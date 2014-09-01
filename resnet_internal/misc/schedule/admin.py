57b7c3dc1a77ccb43bbf60a476d6727340959c39
Alexander Kavanaugh <akavanau@calpoly.edu>
2014-09-01 13:10:01 -0700

    removed old stuff

--- resnet_internal/misc/schedule/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from models import CalHourA, CalDayA, Calendar, StudentCalendar, Tech
-
-admin.site.register(Calendar)
-admin.site.register(StudentCalendar)
-admin.site.register(Tech)
