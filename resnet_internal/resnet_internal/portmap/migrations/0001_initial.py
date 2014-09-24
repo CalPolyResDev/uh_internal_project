dba6cf54f8e6c70fed47148a6822e9fe69c46686
Alexander Kavanaugh <akavanau@calpoly.edu>
2014-09-24 11:47:41 -0700

    removed migrations

--- resnet_internal/resnet_internal/portmap/migrations/0001_initial.py
+++ resnet_internal/resnet_internal/portmap/migrations/0001_initial.py
@@ -1,32 +1,24 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        pass
+    dependencies = [
+    ]
 
-    def backwards(self, orm):
-        pass
-
-    models = {
-        u'portmap.reshallwired': {
-            'Meta': {'object_name': 'ResHallWired', 'db_table': "u'residence_halls_wired'", 'managed': 'False'},
-            'blade': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
-            'building': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
-            'community': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'jack': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
-            'port': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
-            'room': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
-            'switch_ip': ('django.db.models.fields.IPAddressField', [], {'max_length': '15'}),
-            'switch_name': ('django.db.models.fields.CharField', [], {'max_length': '35'}),
-            'vlan': ('django.db.models.fields.CharField', [], {'max_length': '7'})
-        }
-    }
-
-    complete_apps = ['portmap']
+    operations = [
+        migrations.CreateModel(
+            name='ResHallWired',
+            fields=[
+            ],
+            options={
+                'verbose_name': 'Residence Halls Wired Port',
+                'db_table': 'residence_halls_wired',
+                'managed': False,
+            },
+            bases=(models.Model,),
+        ),
+    ]
