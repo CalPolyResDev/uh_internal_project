63442455afacc53a81c834486f8ddeaf5c8e4960
Alexander Kavanaugh <akavanau@calpoly.edu>
2014-09-24 11:47:20 -0700

    Removed dajax/dajaxice as a dependency (RNIN-65).

--- resnet_internal/resnet_internal/core/ajax.py
+++ resnet_internal/resnet_internal/core/ajax.py
@@ -7,45 +7,39 @@
 """
 
 import datetime
-import imaplib
 import logging
 
-from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.contrib.auth import get_user_model
-from django.db import DatabaseError
+from django.views.decorators.http import require_POST
 
-from dajax.core import Dajax
-from dajaxice.decorators import dajaxice_register
-from srsconnector.models import ServiceRequest
+from django_ajax.decorators import ajax
 from rmsconnector.constants import (SIERRA_MADRE, YOSEMITE, SOUTH_MOUNTAIN, NORTH_MOUNTAIN,
                                     CERRO_VISTA, POLY_CANYON_VILLAGE, SIERRA_MADRE_BUILDINGS, YOSEMITE_BUILDINGS,
                                     SOUTH_MOUNTAIN_BUILDINGS, NORTH_MOUNTAIN_BUILDINGS, CERRO_VISTA_BUILDINGS,
                                     POLY_CANYON_VILLAGE_BUILDINGS)
 
 from .models import DailyDuties
+from .utils import GetDutyData
 
 logger = logging.getLogger(__name__)
 
-GREEN = "#060"
-RED = "#900"
 
-ACCEPTABLE_LAST_CHECKED = datetime.timedelta(days=1)
-
-
-@dajaxice_register
-def update_building(request, community, building_selection=None):
+@ajax
+@require_POST
+def update_building(request):
     """ Update building drop-down choices based on the community chosen.
 
     :param community: The community for which to display building choices.
     :type community: str
-
     :param building_selection: The building selected before form submission.
     :type building_selection: str
 
     """
 
-    dajax = Dajax()
+    # Pull post parameters
+    community = request.POST.get("community", None)
+    building_selection = request.POST.get("building_selection", None)
 
     building_options = {
         SIERRA_MADRE: [(building, building) for building in SIERRA_MADRE_BUILDINGS],
@@ -58,28 +52,32 @@ def update_building(request, community, building_selection=None):
     choices = []
 
     # Add options iff a building is selected
-    if str(community) != "":
+    if community:
         for value, label in building_options[str(community)]:
             if building_selection and value == building_selection:
                 choices.append("<option value='%s' selected='selected'>%s</option>" % (value, label))
             else:
                 choices.append("<option value='%s'>%s</option>" % (value, label))
     else:
+        logger.debug("A building wasn't passed via POST.")
         choices.append("<option value='%s'>%s</option>" % ("", "---------"))
 
-    dajax.assign('#id_building', 'innerHTML', ''.join(choices))
+    data = {
+        'inner-fragments': {
+            '#id_building': ''.join(choices),
+        },
+    }
 
-    return dajax.json()
+    return data
 
 
-@dajaxice_register
+@ajax
 def refresh_duties(request):
-    dajax = Dajax()
 
     # Load data dicts
     messages_dict = GetDutyData().get_messages()
     email_dict = GetDutyData().get_email()
-    tickets_dict = GetDutyData().get_tickets()
+    tickets_dict = GetDutyData().get_tickets() # {"count": 0, "status_color": "#900", "last_checked": "", "last_user": ""}
 
     if messages_dict["count"] > 0:
         message_count = u' <b>(' + str(messages_dict["count"]) + ')</b>'
@@ -127,117 +125,31 @@ def refresh_duties(request):
         Note: Times update upon click of task title link.
     </p>"""
 
-    # Update the daily duties widget
-    dajax.assign('#dailyDuties', 'innerHTML', duties_html)
-    dajax.script('update_link_targets("' + str(request.user.open_links_in_frame) + '");')
+    data = {
+        'inner-fragments': {
+            '#dailyDuties': duties_html
+        },
+    }
 
-    return dajax.json()
+    return data
 
 
-@dajaxice_register
-def update_duty(request, duty):
-    dajax = Dajax()
+@ajax
+@require_POST
+def update_duty(request):
+    """ Update a particular duty.
+
+    :param duty: The duty to update.
+    :type duty: str
+
+    """
+
+    # Pull post parameters
+    duty = request.POST["duty"]
 
     data = DailyDuties.objects.get(name=duty)
     data.last_checked = datetime.datetime.now()
     data.last_user = get_user_model().objects.get(username=request.user.username)
     data.save()
 
-    return dajax.json()
-
-
-class GetDutyData:
-
-    def get_messages(self):
-        """Checks the current number of voicemail messages.
-
-        An API for this has not been discovered yet; For now this method always returns zero.
-
-        """
-
-        messages = {
-            "count": None,
-            "status_color": None,
-            "last_checked": None,
-            "last_user": None
-        }
-
-        data = DailyDuties.objects.get(name='messages')
-
-        messages["count"] = 0
-        if data.last_checked > datetime.datetime.now() - ACCEPTABLE_LAST_CHECKED:
-            messages["status_color"] = GREEN
-        else:
-            messages["status_color"] = RED
-        messages["last_checked"] = datetime.datetime.strftime(data.last_checked, "%m/%d/%Y %H:%M%p")
-        messages["last_user"] = data.last_user.get_full_name()
-
-        return messages
-
-    def get_email(self):
-        """Checks the current number of unread email messages."""
-
-        email = {
-            "count": None,
-            "status_color": None,
-            "last_checked": None,
-            "last_user": None
-        }
-        server = None
-
-        # Connect to the email server and authenticate
-        if settings.INCOMING_EMAIL['IMAP4']['USE_SSL']:
-            server = imaplib.IMAP4_SSL(settings.INCOMING_EMAIL['IMAP4']['HOST'], settings.INCOMING_EMAIL['IMAP4']['PORT'])
-        else:
-            server = imaplib.IMAP4(settings.INCOMING_EMAIL['IMAP4']['HOST'], settings.INCOMING_EMAIL['IMAP4']['PORT'])
-        server.login(settings.INCOMING_EMAIL['IMAP4']['USER'], settings.INCOMING_EMAIL['IMAP4']['PASSWORD'])
-
-        # Grab the number of unread emails
-        server.select('inbox', readonly=True)  # Select the Inbox
-        r, search_data = server.search(None, "UnSeen")  # Search for unread emails
-        unread_count = len(search_data[0].split())  # Count unread emails
-        server.logout()  # Disconnect from the email server
-
-        data = DailyDuties.objects.get(name='email')
-
-        email["count"] = unread_count
-        if data.last_checked > datetime.datetime.now() - ACCEPTABLE_LAST_CHECKED:
-            email["status_color"] = GREEN
-        else:
-            email["status_color"] = RED
-        email["last_checked"] = datetime.datetime.strftime(data.last_checked, "%m/%d/%Y %H:%M%p")
-        email["last_user"] = data.last_user.get_full_name()
-
-        return email
-
-    def get_tickets(self):
-        """Checks the current number of queued SRS tickets."""
-
-        tickets = {
-            "count": None,
-            "status_color": None,
-            "last_checked": None,
-            "last_user": None
-        }
-
-        try:
-            open_tickets = ServiceRequest.objects.filter(assigned_team="SA RESNET").exclude(status=4).count()
-            api_tickets = ServiceRequest.objects.filter(assigned_person="ResnetAPI").count()
-
-            data = DailyDuties.objects.get(name='tickets')
-
-            tickets["count"] = open_tickets - api_tickets
-            if data.last_checked > datetime.datetime.now() - ACCEPTABLE_LAST_CHECKED:
-                tickets["status_color"] = GREEN
-            else:
-                tickets["status_color"] = RED
-            tickets["last_checked"] = datetime.datetime.strftime(data.last_checked, "%m/%d/%Y %H:%M%p")
-            tickets["last_user"] = data.last_user.get_full_name()
-        except DatabaseError, message:
-            logger.info(message)
-            tickets["count"] = 0
-            tickets["status_color"] = RED
-            tickets["last_checked"] = datetime.datetime.strftime(datetime.datetime.now(), "%m/%d/%Y %H:%M%p")
-            tickets["last_user"] = "Connection Error!"
-
-        return tickets
+    return refresh_duties
