9184291c56751d25cee328f6de235eb006b240b2
Thomas Willson <thomas.willson@me.com>
2015-09-30 14:28:19 -0700

    Rename jfu escape template tags.

--- resnet_internal/apps/dailyduties/templatetags/jfu_escaped.py
+++ resnet_internal/apps/dailyduties/templatetags/jfu_escaped.py
@@ -4,10 +4,22 @@ Created on Sep 30, 2015
 @author: thomaswillson
 '''
 from django.template import Library
-from django.utils.html import escapejs
+from django.utils.html import escapejs, escape
 from jfu.templatetags.jfutags import jfu
 
 register = Library()
+
+
+@register.simple_tag(takes_context=True)
+def jfu_escaped_js(
+    context,
+    template_name='jfu/upload_form.html',
+    upload_handler_name='jfu_upload',
+    *args, **kwargs
+):
+
+    jfu_response = jfu(context, template_name, upload_handler_name, *args, **kwargs)
+    return escapejs(jfu_response)
 
 
 @register.simple_tag(takes_context=True)
@@ -19,4 +31,4 @@ def jfu_escaped(
 ):
 
     jfu_response = jfu(context, template_name, upload_handler_name, *args, **kwargs)
-    return escapejs(jfu_response)
+    return escape(jfu_response)
