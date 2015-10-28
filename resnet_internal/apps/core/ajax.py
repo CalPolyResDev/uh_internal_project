30ce273928884622b129b28684945fd8c3625abb
Thomas Willson <thomas.willson@me.com>
2015-10-28 11:56:56 -0700

    Remove legacy ajax buildings method.

--- resnet_internal/apps/core/ajax.py
+++ resnet_internal/apps/core/ajax.py
@@ -23,51 +23,6 @@ logger = logging.getLogger(__name__)
 
 
 @ajax
-@require_POST
-def update_building(request):
-    """ Update building drop-down choices based on the community chosen.
-
-    :param community_id: The community for which to display building choices.
-    :type community_id: str
-
-    :param building_selection_id (optional): The building selected before form submission.
-    :type building_selection_id (optional): str
-
-    :param css_target (optional): The target of which to replace inner HTML. Defaults to #id_sub_department.
-    :type css_target (optional): str
-
-    """
-
-    # Pull post parameters
-    community_id = request.POST.get("community_id", None)
-    building_selection_id = request.POST.get("building_selection_id", None)
-    css_target = request.POST.get("css_target", '#id_sub_department')
-
-    choices = []
-
-    # Add options iff a department is selected
-    if community_id:
-        community_instance = Community.objects.get(id=int(community_id))
-
-        for building in community_instance.buildings.all():
-            if building_selection_id and building.id == int(building_selection_id):
-                choices.append("<option value='{id}' selected='selected'>{name}</option>".format(id=building.id, name=building.name))
-            else:
-                choices.append("<option value='{id}'>{name}</option>".format(id=building.id, name=building.name))
-    else:
-        logger.warning("A department wasn't passed via POST.")
-        choices.append("<option value='{id}'>{name}</option>".format(id="", name="---------"))
-
-    data = {
-        'inner-fragments': {
-            css_target: ''.join(choices)
-        },
-    }
-
-    return data
-
-
-@ajax
 def update_network_status(request):
     network_reachability = NetworkReachabilityTester.get_network_device_reachability()
     network_reachability.sort(key=itemgetter('status', 'display_name'))
--- resnet_internal/urls.py
+++ resnet_internal/urls.py
@@ -24,7 +24,7 @@ from .apps.adgroups.ajax import remove_resnet_tech
 from .apps.adgroups.views import ResTechListEditView
 from .apps.computers.ajax import PopulateComputers, UpdateComputer, update_sub_department, remove_computer, remove_pinhole, remove_domain_name
 from .apps.computers.views import ComputersView, ComputerRecordsView, RDPRequestView, PinholeRequestView, DomainNameRequestView
-from .apps.core.ajax import update_building, update_network_status, get_tickets, BuildingChainedAjaxView, RoomChainedAjaxView
+from .apps.core.ajax import update_network_status, get_tickets, BuildingChainedAjaxView, RoomChainedAjaxView
 from .apps.core.views import IndexView, handler500, TicketSummaryView
 from .apps.dailyduties.ajax import refresh_duties, update_duty, remove_voicemail, get_email_folders, get_mailbox_summary, email_mark_unread, email_mark_read, email_archive, send_email, attachment_upload, attachment_delete
 from .apps.dailyduties.views import VoicemailListView, VoicemailAttachmentRequestView, EmailMessageView, EmailListView, EmailAttachmentRequestView, EmailComposeView
@@ -91,7 +91,6 @@ urlpatterns = [
     url(r'^flugzeug/', include(admin.site.urls)),  # admin site urls, masked
     url(r'^login/$', auth_login, name='login'),
     url(r'^logout/$', auth_logout, name='logout', kwargs={'next_page': settings.CAS_LOGOUT_URL}),
-    url(r'^ajax/update_building/$', update_building, name='ajax_update_building'),
     url(r'^ajax/chained_building/$', login_required(BuildingChainedAjaxView.as_view()), name='core_chained_building'),
     url(r'^ajax/chained_room/$', login_required(RoomChainedAjaxView.as_view()), name='core_chained_room'),
     url(r'^core/network_status/update/$', update_network_status, name='core_update_network_status'),
