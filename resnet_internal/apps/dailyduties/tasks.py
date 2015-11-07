dd5b54f33d8561d33666fc603abb3749cdaf6fe7
Thomas Willson <thomas.willson@me.com>
2015-11-07 13:56:43 -0800

    Fix email slack notification reverse lookup.

--- resnet_internal/apps/dailyduties/tasks.py
+++ resnet_internal/apps/dailyduties/tasks.py
@@ -64,7 +64,7 @@ def update_slack_email(num):
                     'color': 'good',
                     'author_name': email['from_name'] + ' (' + email['from_address'] + ')',
                     'title': 'Subject: ' + email['subject'],
-                    'title_link': urljoin(settings.DEFAULT_BASE_URL, reverse('email_view_message', mailbox_name='INBOX', uid=email['uid'])),
+                    'title_link': urljoin(settings.DEFAULT_BASE_URL, reverse('email_view_message', kwargs={'mailbox_name': 'INBOX', 'uid': email['uid']})),
                     'text': email_message['body_plain_text'] if email_message['body_plain_text'] else html2text(email_message['body_html']),
 
                 }
