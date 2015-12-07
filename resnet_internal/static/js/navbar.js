ff2aae63b2debdb51384d8323b570570eeb34b0a
Thomas Willson <thomas.willson@me.com>
2015-12-07 10:34:14 -0800

    Move navbar javascript to separate file.

---
+++ resnet_internal/static/js/navbar.js
@@ -0,0 +1,45 @@
+$("#drawer").collapse();
+
+$("#drawer-trigger").on("click", function() {
+    if (window.innerWidth < 615) {
+        $("#drawer").collapse("toggle");
+    }
+});
+
+$(window).on("load resize", function() {
+    if (window.innerWidth < 615) {
+        $("#drawer").collapse("hide");
+    } else {
+        $("#drawer").collapse("show");
+    }
+});
+
+$('#drawer').on('hidden.bs.collapse', function() {
+    $("#drawer-trigger .glyphicon").removeClass("glyphicon-menu-up");
+
+    if (window.innerWidth < 615) {
+        $("#drawer-trigger .glyphicon").addClass("glyphicon-menu-down");
+    }
+});
+
+$('#drawer').on('shown.bs.collapse', function() {
+    $("#drawer-trigger .glyphicon").removeClass("glyphicon-menu-down");
+
+    if (window.innerWidth < 615) {
+        $("#drawer-trigger .glyphicon").addClass("glyphicon-menu-up");
+    } else {
+        $("#drawer-trigger .glyphicon").removeClass("glyphicon-menu-up");
+    }
+});
+
+ $(".collapsable").on('hidden.bs.collapse', function(event) {
+    var trigger_id = $(event.target).attr("id").replace("_list", "_link");
+    $('#' + trigger_id + ' .glyphicon').removeClass("glyphicon-menu-up");
+    $('#' + trigger_id + ' .glyphicon').addClass("glyphicon-menu-down");
+});
+
+$(".collapsable").on('shown.bs.collapse', function(event) {
+    var trigger_id = $(event.target).attr("id").replace("_list", "_link");
+    $('#' + trigger_id + ' .glyphicon').removeClass("glyphicon-menu-down");
+    $('#' + trigger_id + ' .glyphicon').addClass("glyphicon-menu-up");
+});
