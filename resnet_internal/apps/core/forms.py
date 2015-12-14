74db929d0306d80bd7fbbeb6e85a833b73350e4e
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2015-12-14 07:24:43 +0000

    resnet technician list complete

--- resnet_internal/apps/core/forms.py
+++ resnet_internal/apps/core/forms.py
@@ -26,7 +26,7 @@ class RoomCreateForm(ChainedChoicesModelForm):
         self.helper.form_method = 'post'
         self.helper.html5_required = True
 
-        self.helper.form_class = 'form-horizontal datatable-add-form'
+        self.helper.form_class = 'form-horizontal table-add-form'
         self.helper.label_class = 'col-sm-2'
         self.helper.field_class = 'col-sm-10 col-md-8'
 
