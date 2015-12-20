0801e29eea0d9ee0288ebde9d678d5872ea7b0df
Thomas Willson <thomas.willson@me.com>
2015-12-19 22:59:44 -0800

    Update django-clever-selects client hostname.

--- resnet_internal/settings/production.py
+++ resnet_internal/settings/production.py
@@ -17,7 +17,7 @@ ALLOWED_HOSTS = [
     'dev.resdev.calpoly.edu',
     'prod.resdev.calpoly.edu',
     '.internal.resnet.calpoly.edu',
-    'testserver',  # Needed for django-clever-selects for some reason.
+    'clever_selects_client',  # Needed for django-clever-selects for form repopulation.
 ]
 
 DEFAULT_BASE_URL = 'https://internal.resnet.calpoly.edu'
