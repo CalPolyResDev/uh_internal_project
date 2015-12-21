c0112b236a031199531455a8a0e31c9960770329
Mordor ResDev RNIN <resnet@calpoly.edu>
2015-12-21 11:50:54 -0800

    Add better logging of connection reinitialization.

--- resnet_internal/apps/dailyduties/utils.py
+++ resnet_internal/apps/dailyduties/utils.py
@@ -107,8 +107,9 @@ class EmailConnectionMixin(object):
                 connection.login(username, password)
                 connection.select_folder('INBOX')
                 connection.close_folder()
-            except (OSError, IMAP4.error):  # Office 365 seems to randomly reject connections and trying again usually results in a connection.
+            except (OSError, IMAP4.error) as exc:  # Office 365 seems to randomly reject connections and trying again usually results in a connection.
                 connection = None
+                logger.warning('Trying to get connection again: ' + str(exc), exc_info=True)
 
         return connection
 
