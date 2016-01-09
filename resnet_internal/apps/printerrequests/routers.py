9c8276e7dcdfccbf3b6d82c5a2ec7bbb46b76ddc
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2016-01-09 00:50:40 +0000

    working router fix

--- resnet_internal/apps/printerrequests/routers.py
+++ resnet_internal/apps/printerrequests/routers.py
@@ -17,27 +17,28 @@ class PrinterRequestsRouter(object):
     def db_for_read(self, model, **hints):
         """Routes database read requests to the printer request database."""
 
-        if model._meta.app_label == self.APP_NAME and model._meta.name != self.EXCLUDED_MODEL:
+        if model._meta.app_label == self.APP_NAME and model._meta.model_name != self.EXCLUDED_MODEL:
+            print(model._meta.name)
             return self.DATABASE_ALIAS
         return None
 
     def db_for_write(self, model, **hints):
         """Routes database write requests to the printer request database."""
 
-        if model._meta.app_label == self.APP_NAME and model._meta.name != self.EXCLUDED_MODEL:
+        if model._meta.app_label == self.APP_NAME and model._meta.model_name != self.EXCLUDED_MODEL:
             return self.DATABASE_ALIAS
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
         """Allow relations if a printer request model is involved."""
 
-        if (obj1._meta.app_label == self.APP_NAME or obj2._meta.app_label == self.APP_NAME) and obj1._meta.name != self.EXCLUDED_MODEL and obj2._meta.name != self.EXCLUDED_MODEL:
+        if (obj1._meta.app_label == self.APP_NAME or obj2._meta.app_label == self.APP_NAME) and obj1._meta.model_name != self.EXCLUDED_MODEL and obj2._meta.model_name != self.EXCLUDED_MODEL:
             return True
         return None
 
     def allow_migrate(self, db, app_label, model=None, **hints):
         """Disallow RMS migrations."""
 
-        if app_label == self.APP_NAME and model._meta.name != self.EXCLUDED_MODEL:
+        if app_label == self.APP_NAME and model._meta.model_name != self.EXCLUDED_MODEL:
             return False
         return None
