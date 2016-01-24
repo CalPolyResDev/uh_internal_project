1ba33bcfd26fb4e1962a71343e979b72f3a70818
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2016-01-23 19:03:09 -0800

    renamed more files to .djhtml, removed unused datatables images

--- resnet_internal/apps/portmap/views.py
+++ resnet_internal/apps/portmap/views.py
@@ -29,7 +29,7 @@ class PortsView(ChainedSelectFormViewMixin, DatatablesView):
 
 
 class AccessPointsView(ChainedSelectFormViewMixin, DatatablesView):
-    template_name = "portmap/access_points.html"
+    template_name = "portmap/access_points.djhtml"
     form_class = AccessPointCreateForm
     populate_class = PopulateAccessPoints
     model = AccessPoint
