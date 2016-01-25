9743f0a5ab1f6741b91a8192f35ae71a34f96614
Thomas Willson <tewillso@calpoly.edu>
2016-01-25 11:26:37 -0800

    Use PermissionDenied for non-developers logging onto the staging server.
    Previously used PermissionError which is not the proper exception for Django.

--- resnet_internal/apps/core/backends.py
+++ resnet_internal/apps/core/backends.py
@@ -84,7 +84,7 @@ class CASLDAPBackend(CASBackend):
                     raise PermissionDenied('User %s is not in any of the allowed groups.' % principal_name)
 
                 if not user.ad_groups.all().filter(distinguished_name='CN=UH-RN-DevTeam,OU=ResNet,OU=UH,OU=Manual,OU=Groups,DC=ad,DC=calpoly,DC=edu').exists() and settings.RESTRICT_LOGIN_TO_DEVELOPERS:
-                    raise PermissionError('Only developers can access the site on this server. Please use the primary site.')
+                    raise PermissionDenied('Only developers can access the site on this server. Please use the primary site.')
 
                 # Legacy Permissions Flags
                 net_admin_list = get_group_members('CN=StateHRDept - IS-ITS-Networks (132900 FacStf Only),OU=FacStaff,OU=StateHRDept,OU=Automated,OU=Groups,DC=ad,DC=calpoly,DC=edu')
