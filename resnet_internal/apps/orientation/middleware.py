234423d0f1680805da08e2032a88993a0dc00695
Thomas Willson <tewillso@calpoly.edu>
2016-01-29 12:02:20 -0800

    Anonymous user fix.

--- resnet_internal/apps/orientation/middleware.py
+++ resnet_internal/apps/orientation/middleware.py
@@ -14,7 +14,7 @@ class OrientationRedirectMiddleware(object):
     def process_request(self, request):
         if 'orientation' not in request.path and 'flugzeug' not in request.path:
             user = getattr(request, 'user', None)
-            if user and (not user.orientation_complete and user.is_technician):
+            if user.is_authenticated() and (not user.orientation_complete and user.is_technician):
                 return HttpResponseRedirect(reverse_lazy('orientation_checklist'))
 
         return None
