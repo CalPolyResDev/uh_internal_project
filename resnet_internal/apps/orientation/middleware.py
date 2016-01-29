f8fd80dfbe1454107fbeb0366da2512a4d2568b9
Thomas Willson <tewillso@calpoly.edu>
2016-01-28 18:12:00 -0800

    User orientation detection fix.
    Non-technicians don't need to complete orientation.

--- resnet_internal/apps/orientation/middleware.py
+++ resnet_internal/apps/orientation/middleware.py
@@ -14,7 +14,7 @@ class OrientationRedirectMiddleware(object):
     def process_request(self, request):
         if 'orientation' not in request.path and 'flugzeug' not in request.path:
             user = getattr(request, 'user', None)
-            if user is not None and not user.orientation_complete:
+            if user and (not user.orientation_complete and user.is_technician):
                 return HttpResponseRedirect(reverse_lazy('orientation_checklist'))
 
         return None
