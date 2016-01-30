CalPolyResDev/uh_internal_project

commit f7f19a93c6b5aa3ad943000b3181a357ea2637a9

Add better commenting regarding the CC a CSD functionality.

resnet_internal/static/js/email_message.js
@@ -60,7 +60,6 @@ function change_to_editor() {
         html: true
     });
     
-    $('#id_community').attr('onchange', '$(this).loadAllChainedChoices();');
 
     add_button('CC a CSD', '', 'cc_csd_button');
     $('#cc_csd_button').attr('title', 'Select Building:');
@@ -77,6 +76,15 @@ function change_to_editor() {
     });
     
     // Not so clever selects...
+    // Clever Selects has several issues within a popover:
+    //      1. It doesn't register the event handler. The workaround is to manually add on onchange attribute to the field.
+    //      2. Even then, the child field is updated in the source DOM, not in the popover. Therefore, the below code
+    //         adds an observer for these changes then copies them to the popover.
+    //      3. Something causes the chained_ids attribute of the parent field to be appended to everytime the parent
+    //         value changes. Therefore, there is code to reset this attribute back to a singular 'id_building'. 
+
+    $('#id_community').attr('onchange', '$(this).loadAllChainedChoices();');
+
     var ccCSDMutationObserver = new MutationObserver(function(mutations, observer) {
         var selected_community = $('.popover-content > div > form > fieldset > div > div > #id_community').val();
         $('#cc_csd_popover_content > div > form > fieldset > div > div > #id_community').attr('chained_ids', 'id_building');

resnet_internal/templates/dailyduties/email_viewer.html
@@ -113,6 +113,11 @@
     <div style="display: none;" id="cc_csd_popover_content">
         <div>
             {% crispy form %}
+            <!-- Adding to the issues surrounding the clever selects, it seems impossible to
+                use a standard submit button to the form. jQuery was used to try and add submit
+                event listeners and onsubmit attributes neither of which resulted in the submit
+                function being called. Therefore the button is added below with the onclick
+                handler specified. -->
             <button class="btn btn-primary" onclick="submit_cc_csd_form();">Add</button>
         </div>
     </div>

commit 1294c69ad1cebf7bf54f1f93433baed20e37f86d

Fix CC CSD regression introduced in f7f19a93c6b5aa3ad943000b3181a357ea2637a9.

resnet_internal/static/js/email_message.js
@@ -60,6 +60,7 @@ function change_to_editor() {
         html: true
     });
     
+    $('#id_community').attr('onchange', '$(this).loadAllChainedChoices();');
 
     add_button('CC a CSD', '', 'cc_csd_button');
     $('#cc_csd_button').attr('title', 'Select Building:');
@@ -70,16 +71,16 @@ function change_to_editor() {
     var cc_csd_popover = $('#cc_csd_button').popover({
         html: true
     });
-    
+
     // Not so clever selects...
+
     // Clever Selects has several issues within a popover:
-    //      1. It doesn't register the event handler. The workaround is to manually add on onchange attribute to the field.
+    //      1. It doesn't register the event handler. The workaround is to manually add on onchange attribute to the field
+    //         which is handled before the button is added above.
     //      2. Even then, the child field is updated in the source DOM, not in the popover. Therefore, the below code
     //         adds an observer for these changes then copies them to the popover.
     //      3. Something causes the chained_ids attribute of the parent field to be appended to everytime the parent
     //         value changes. Therefore, there is code to reset this attribute back to a singular 'id_building'. 
-
-    $('#id_community').attr('onchange', '$(this).loadAllChainedChoices();');
 
     var ccCSDMutationObserver = new MutationObserver(function(mutations, observer) {
         var selected_community = $('.popover-content > div > form > fieldset > div > div > #id_community').val();