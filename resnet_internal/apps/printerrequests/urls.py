ac9fe9b8a1d885a1b22fbf5775dbf218eaec5aac
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2016-03-13 18:15:15 -0700

    reordered up printer request urls (RNIN-186)

--- resnet_internal/apps/printerrequests/urls.py
+++ resnet_internal/apps/printerrequests/urls.py
@@ -16,15 +16,16 @@ from .views import RequestsListView, InventoryView, OnOrderView, RequestTonerVie
 app_name = 'printerrequests'
 
 urlpatterns = [
-    url(r'^$', login_required(daily_duties_access(RequestsListView.as_view())), name='home'),
-    url(r'^view_inventory/$', login_required(daily_duties_access(InventoryView.as_view())), name='inventory'),
-    url(r'^view_ordered/$', login_required(daily_duties_access(OnOrderView.as_view())), name='ordered_items'),
+    url(r'^$', login_required(printer_request_create_access(RequestsListView.as_view())), name='home'),
+    url(r'^change_status/$', login_required(daily_duties_access(change_request_status)), name='change_status'),
+
     url(r'^toner/', login_required(printer_request_create_access(RequestTonerView.as_view())), name='toner_request'),
     url(r'^parts/', login_required(printer_request_create_access(RequestPartsView.as_view())), name='parts_request'),
-    url(r'^change_status/$', login_required(daily_duties_access(change_request_status)), name='change_status'),
-    url(r'^toner/update_inventory/$', login_required(daily_duties_access(update_toner_inventory)), name='update_toner_inventory'),
-    url(r'^parts/update_inventory/$', login_required(daily_duties_access(update_part_inventory)), name='update_part_inventory'),
-
     url(r'^ajax/update_toner/', login_required(printer_request_create_access(update_toner)), name='ajax_update_printer_toner_cartridge'),
     url(r'^ajax/update_part/', login_required(printer_request_create_access(update_part)), name='ajax_update_printer_part'),
+
+    url(r'^view_inventory/$', login_required(daily_duties_access(InventoryView.as_view())), name='inventory'),
+    url(r'^view_ordered/$', login_required(daily_duties_access(OnOrderView.as_view())), name='ordered_items'),
+    url(r'^toner/update_inventory/$', login_required(daily_duties_access(update_toner_inventory)), name='update_toner_inventory'),
+    url(r'^parts/update_inventory/$', login_required(daily_duties_access(update_part_inventory)), name='update_part_inventory'),
 ]
