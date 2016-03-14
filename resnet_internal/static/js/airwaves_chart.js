7935104b3521ecea9672e0d4da7a1961ef5cd15f
Thomas Willson <tewillso@calpoly.edu>
2016-03-13 20:01:08 -0700

    Better formatting of chart tooltip.

--- resnet_internal/static/js/airwaves_chart.js
+++ resnet_internal/static/js/airwaves_chart.js
@@ -1,3 +1,34 @@
+// Python-Style string formatting
+// Source: http://stackoverflow.com/a/18234317
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        var str = this.toString();
+        if (!arguments.length)
+            return str;
+        var args = typeof arguments[0],
+            args = (("string" == args || "number" == args) ? arguments : arguments[0]);
+        for (arg in args)
+            str = str.replace(RegExp("\\{" + arg + "\\}", "gi"), args[arg]);
+        return str;
+    }
+}
+
+function humanFileSize(bytes, si) {
+    var thresh = si ? 1000 : 1024;
+    if(Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = si
+        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1)+' '+units[u];
+}
+
 function displayAirwavesChart(jquerySelector, datasourceURL) {
     var date = new Date();
     Highcharts.setOptions({
@@ -30,7 +61,15 @@ function displayAirwavesChart(jquerySelector, datasourceURL) {
             credits: {
                 enabled: false
             },
-            series: response.data,  
+            series: response.data,
+            tooltip: {
+                formatter: function() {
+                    return '<span style="color:{pointColor}">\u25CF</span> {seriesName}: <b>{value}</b><br/>'.format({
+                     pointColor: this.color,
+                     seriesName: this.series.name,
+                     value: this.series.options.chart_type.indexOf('bps') > -1 ? humanFileSize(this.y, true) : this.y,
+                });},
+            },
         });
     });
 }
