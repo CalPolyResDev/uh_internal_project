e8285c8bc41e46b2ca3dbd2ca6898ac8f8cc445a
Thomas Willson <tewillso@calpoly.edu>
2016-04-11 14:46:51 -0700

    Use producer-consumer architecture for ClearPass syslog server.

--- uh_internal/apps/network/clearpass/syslog_server.py
+++ uh_internal/apps/network/clearpass/syslog_server.py
@@ -17,18 +17,36 @@ from .parser import parse_login_attempts
 logger = logging.getLogger(__name__)
 
 
+def worker(packet_queue):
+    while True:
+        try:
+            packet = packet_queue.get(True, 60)
+        except packet_queue.Empty:
+            pass
+
+        if packet.get('shutdown', False):
+            return
+
+        parse_login_attempts(packet['data'])
+
+
 class SyslogUDPHandler(socketserver.BaseRequestHandler):
     """Based on https://gist.github.com/marcelom/4218010"""
+
+    def __init__(self, request, client_address, server, queue):
+        self.queue = queue
+        return super().__init__(request, client_address, server)
 
     def handle(self):
         try:
             if self.client_address[0] in settings.CLEARPASS_SERVERS:
                 data = bytes.decode(self.request[0].strip())
-                parse_login_attempts(data)
+                self.queue.put({'data': data})
             else:
                 logger.warning('Throwing away packet from ' + str(self.client_address[0]))
         except KeyboardInterrupt:
             print('Exiting...')
+            self.queue.put({'shutdown': True})
             exit(0)
         except Exception as e:
             client.captureException()
--- uh_internal/apps/network/management/commands/run_clearpass_syslog_server.py
+++ uh_internal/apps/network/management/commands/run_clearpass_syslog_server.py
@@ -7,16 +7,34 @@
 .. moduleauthor:: Thomas Willson <thomas.willson@icloud.com>
 
 """
+from multiprocessing import Queue, Process
 import socketserver
 
 from django.core.management.base import BaseCommand
+from django.db import connection
+
+
+class LargePacketUDPServer(socketserver.UDPServer):
+    request_queue_size = 5000
+    max_packet_size = 30000
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args)
+        self.queue = kwargs['queue']
+
+    def finish_request(self, request, client_address):
+        self.RequestHandlerClass(request, client_address, self, self.queue)
 
 
 class Command(BaseCommand):
     can_import_settings = True
 
     def handle(self, *args, **options):
-        from ...clearpass.syslog_server import SyslogUDPHandler  # noqa
+        from ...clearpass.syslog_server import SyslogUDPHandler, worker  # noqa
+        queue = Queue(2000)
+        worker_process = Process(target=worker, args=(queue,))
+        connection.close()
+        worker_process.start()
 
-        server = socketserver.UDPServer(('0.0.0.0', 5140), SyslogUDPHandler)
+        server = LargePacketUDPServer(('0.0.0.0', 5140), SyslogUDPHandler, queue=queue)
         server.serve_forever()
