cf65ae39426a27f36f0cbde096fef27b49cd8391
Thomas Willson <tewillso@calpoly.edu>
2016-07-04 13:32:06 -0700

    Fix task import in wsgi.

--- uh_internal/wsgi.py
+++ uh_internal/wsgi.py
@@ -11,6 +11,7 @@ from raven.contrib.django.raven_compat.middleware.wsgi import Sentry  # noqa
 django.setup()
 
 # Import any functions with uWSGI decorators here:
-from .apps.reservations import tasks  # noqa
+from uh_internal.apps.dailyduties.tasks import update_slack_voicemail, update_slack_email, prune_expired_email_viewer_records  # noqa
+from uh_internal.apps.network.tasks import update_slack_network_status  # noqa
 
 application = Sentry(WSGIHandler())
