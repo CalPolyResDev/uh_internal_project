6837549e8b3541d9bcbec0cfb8b4fa35610230db
henryluengas <hluengas@calpoly.edu>
2018-02-21 14:09:18 -0800

    The dailyduties admin.py file should not have been changed. I reverted it.

--- uh_internal/apps/dailyduties/admin.py
+++ uh_internal/apps/dailyduties/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import DailyDuties, EmailPermalink
+from .models import DailyDuties, EmailPermalink, EmailViewingRecord
 
 
 class DailyDutiesAdmin(admin.ModelAdmin):
@@ -11,5 +11,10 @@ class EmailPermalinkAdmin(admin.ModelAdmin):
     list_display = ('sender_name', 'sender_email', 'subject', 'slug')
 
 
+class EmailViewingRecordAdmin(admin.ModelAdmin):
+    list_display = ('user', 'mailbox', 'uid', 'replying', 'expiry_time')
+
+
 admin.site.register(DailyDuties, DailyDutiesAdmin)
 admin.site.register(EmailPermalink, EmailPermalinkAdmin)
+admin.site.register(EmailViewingRecord, EmailViewingRecordAdmin)
