b5051677d66d03bdffab8a5cf1ec683c988cf724
henryluengas <hluengas@calpoly.edu>
2018-03-02 13:43:55 -0800

    Removed the emailpermalink model from dailyduties/admin & models.

--- uh_internal/apps/dailyduties/admin.py
+++ uh_internal/apps/dailyduties/admin.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
 
-from .models import DailyDuties, EmailPermalink
+from .models import DailyDuties
 
 
 class DailyDutiesAdmin(admin.ModelAdmin):
     list_display = ('name', 'last_checked', 'last_user')
 
 
-class EmailPermalinkAdmin(admin.ModelAdmin):
-    list_display = ('sender_name', 'sender_email', 'subject', 'slug')
-
-
 admin.site.register(DailyDuties, DailyDutiesAdmin)
-admin.site.register(EmailPermalink, EmailPermalinkAdmin)
