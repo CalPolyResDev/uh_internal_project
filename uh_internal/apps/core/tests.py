d330cce6cb56ddccdac8f4bab73598c2f7cfc95e
fedorareis <fedorareis@gmail.com>
2018-04-30 17:04:51 -0700

    Finished uploaders testing

--- uh_internal/apps/core/tests.py
+++ uh_internal/apps/core/tests.py
@@ -1,35 +1,47 @@
 """
-.. module:: uh_internal.core.test_models
-   :synopsis: uh_internal Core Models Tests.
+.. module:: uh_internal.core.tests
+   :synopsis: uh_internal Core Tests.
 .. moduleauthor:: Kyle Reis (@kjreis)
 """
 
-from django.test.testcases import TestCase
+from django.test import TestCase
 
 from .models import Community, Building
 
 class CommunityTestCase(TestCase):
+    """ Tests the Community model from models.py """
+
     def setUp(self):
         Community.objects.create(name="ResNet")
 
     def test_community_str(self):
+        """ Tests the Community __str__ function """
+
         resnet = Community.objects.get(name="ResNet")
         self.assertEqual(str(resnet), "ResNet")
 
     def test_communities_address(self):
+        """ Tests the Community address property """
+
         resnet = Community.objects.get(name="ResNet")
         self.assertEqual(resnet.address, "ResNet")
 
 
 class BuildingTestCase(TestCase):
+    """ Tests the Building model from models.py """
+
     def setUp(self):
         community = Community.objects.create(name="ResNet")
         Building.objects.create(name="office", community=community)
 
     def test_building_str(self):
+        """ Tests the Building __str__ function """
+
         resnet = Building.objects.get(name="office")
-        self.assertEquals(str(resnet), "office")
+        self.assertEqual(str(resnet), "office")
 
     def test_building_address(self):
+        """ Tests the Building address property """
+
         resnet = Building.objects.get(name="office")
-        self.assertEquals(resnet.address, "ResNet office")
+        self.assertEqual(resnet.address, "ResNet office")
---
+++ uh_internal/apps/uploaders/tests.py
@@ -0,0 +1,63 @@
+"""
+.. module:: uh_internal.uploaders.tests
+   :synopsis: uh_internal Uploaders Tests.
+.. moduleauthor:: Kyle Reis (@kjreis)
+"""
+
+from datetime import datetime
+from django.test import TestCase, RequestFactory
+
+from .models import Uploaders
+from .ajax import log_uploader
+
+class UploadersTestCase(TestCase):
+    """ Tests the functionality of models.py """
+
+    def setUp(self):
+        Uploaders.objects.create(name="test",
+                                 last_run=datetime(2018, 4, 27, 8, 20),
+                                 successful=True)
+        Uploaders.objects.create(name="test2",
+                                 last_run=datetime(2018, 4, 27, 8, 20),
+                                 successful=False)
+
+    def test_uploader_success_str(self):
+        """ Tests a successful uploads print string """
+
+        uploader = Uploaders.objects.get(name="test")
+        self.assertEqual(str(uploader), "test Uploaded on " + str(datetime(2018, 4, 27, 8, 20)))
+
+    def test_uploader_fail_str(self):
+        """ Tests a failed uploads print string """
+
+        uploader = Uploaders.objects.get(name="test2")
+        self.assertEqual(str(uploader), "test2 Failed on " + str(datetime(2018, 4, 27, 8, 20)))
+
+class UploadersAJAXTestCase(TestCase):
+    """ Tests the functionality of ajax.py """
+
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+
+    def test_log_uploader_success(self):
+        """ Tests if a successful upload is logged correctly """
+
+        body = {"uploader": "test2",
+                "success": True}
+        request = self.factory.post('/uploaders/log_upload/', body)
+        response = log_uploader(request)
+        self.assertEqual(response.status_code, 200)
+        uploader = Uploaders.objects.get(name="test2")
+        self.assertEqual(str(uploader)[:-8], "test2 Uploaded on " + str(datetime.now())[:-8])
+
+    def test_log_uploader_fail(self):
+        """ Tests if a failed upload is logged correctly """
+
+        body = {"uploader": "test",
+                "success": False}
+        request = self.factory.post('/uploaders/log_upload/', body)
+        response = log_uploader(request)
+        self.assertEqual(response.status_code, 200)
+        uploader = Uploaders.objects.get(name="test")
+        self.assertEqual(str(uploader)[:-8], "test Failed on " + str(datetime.now())[:-8])
